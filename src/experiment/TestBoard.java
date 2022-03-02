b1c05f633fff8eb849e4e7b823f54649bf8584cd
ScheetzBen <scheetzben@gmail.com>
2022-03-01 17:16:55 -0700

    Added TestBoard Class

---
+++ src/experiment/TestBoard.java
@@ -0,0 +1,21 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoard {
+	public TestBoard() {
+		super();
+	}
+	
+	void calcTargets(TestBoardCell startCell, int pathlength) {
+		return;
+	}
+	
+	Set<TestBoardCell> getTargets() {
+		return null;
+	}
+	
+	TestBoardCell getCell(int row, int col) {
+		return null;
+	}
+}
