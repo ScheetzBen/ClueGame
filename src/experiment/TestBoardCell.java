5d2f23a517b3fc634d770943ccecce9918344471
ScheetzBen <scheetzben@gmail.com>
2022-03-01 17:03:58 -0700

    Finished TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,39 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row, column;
+	private Set<TestBoardCell> adjacencies;
+	private boolean isRoom = false, isOccupied = false;
+	
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	void addAdjacency(TestBoardCell cell) {
+		adjacencies.add(cell);
+	}
+	
+	Set<TestBoardCell> getAdjList() {
+		return adjacencies;
+	}
+	
+	void setRoom(boolean isRoom) {
+		this.isRoom = isRoom;
+	}
+	
+	boolean isRoom() {
+		return isRoom;
+	}
+	
+	void setOccupied(boolean isOccupied) {
+		this.isOccupied = isOccupied;
+	}
+	
+	boolean isOccupied() {
+		return isOccupied;
+	}
 }
