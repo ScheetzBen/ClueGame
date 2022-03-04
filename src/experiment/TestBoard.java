9cf8203fc7345893a9cf056d9d5ccde413fc8530
ScheetzBen <scheetzben@gmail.com>
2022-03-03 18:18:54 -0700

    Started test board Constructor2

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,12 +1,32 @@
 package experiment;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
 
 public class TestBoard {
+	Set<TestBoardCell> board = new HashSet<TestBoardCell>();
+	private static int ROWS = 26, COLUMNS = 23;
+	
 	public TestBoard() {
 		super();
 		
-		
+		// code for when we need to 
+//		try {
+//			FileReader reader = new FileReader("ClueLayout.csv");
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		Scanner in = new Scanner("ClueLayout.csv");
+//		in.useDelimiter(",");
+//		
+//		while (in.hasNext()) {
+//			int currRow = 0, currColumn = 0;
+//			TestBoardCell currCell = new TestBoardCell(currRow, currColumn);
+//			if (in.next() == "X" || in.next() == "W") board.add(currCell);
+//			
+//		}
 	}
 	
 	public void calcTargets(TestBoardCell startCell, int pathlength) {
