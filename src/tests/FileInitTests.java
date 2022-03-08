48642f96ca6ac245c9171540f4936a2c52b871f4
ScheetzBen <scheetz@mines.edu>
2022-03-07 19:26:35 -0700

    Changed everthing in FileInitTests to match our files

--- src/tests/FileInitTests.java
+++ src/tests/FileInitTests.java
@@ -15,7 +15,7 @@ public class FileInitTests {
 	// Constants that I will use to test whether the file was loaded correctly
 		public static final int LEGEND_SIZE = 11;
 		public static final int NUM_ROWS = 25;
-		public static final int NUM_COLUMNS = 24;
+		public static final int NUM_COLUMNS = 23;
 
 		// NOTE: I made Board static because I only want to set it up one
 		// time (using @BeforeAll), no need to do setup before each test.
